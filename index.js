636941ec47f0ceceb239ef686e3951572d894d84
Cow Lols <121976328+CowLols@users.noreply.github.com>
2023-02-05 13:24:15 -0500

    variables

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-console.log("Test");
-console.log("ing");
+//console.log("Test");
+//console.log("ing");
 
-window.alert("Alert!")
+//window.alert("Alert!")
 
 //comment
 
@@ -12,3 +12,16 @@ a
 a
 a
 */
+//let count = 64; //number
+//let name = "Box"; //string
+//let heavy = true; //booleans
+//console.log(name);
+//console.log(count);
+//count = count + 1;
+//console.log("There are",count,name,"!");
+
+//console.log(count)
+//count = count + 1;
+//document.getElementById("p1".innerhtml = "item:" + name);
+//document.getElementById("p2".innerhtml = "count: " + count);
+//document.getElementById("p3".innerhtml = "heavy?" +  heavy);
