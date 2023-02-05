30b44bbfe04a9dcaa873dc39977e5f080f4f0e82
Cow Lols <121976328+CowLols@users.noreply.github.com>
2023-02-05 17:06:51 -0500

    type conversion
    type conversion

--- index.js
+++ index.js
@@ -1,82 +1,22 @@
-//console.log("Test");
-//console.log("ing");
-
-//window.alert("Alert!")
-
-//comment
-
-/*
-a
-a
-a
-a
-a
-*/
+//type conversion 0 change the datatype or value to another
 
 
-//let count = 64; //number
-//let name = "Box"; //string
-//let heavy = true; //booleans
-//console.log(name);
-//console.log(count);
-//count = count + 1;
-//console.log("There are",count,name,"!");
+//let age = window.prompt("how old are you");
 
-//console.log(count)
-//count = count + 1;
-//document.getElementById("p1".innerhtml = "item:" + name);
-//document.getElementById("p2".innerhtml = "count: " + count);
-//document.getElementById("p3".innerhtml = "heavy?" +  heavy);
+//age += 1;
+//age = Number(age);
+//console.log(typeof age);
 
-//arithmetic expression
+//console.log("Your age increased by 1! Your age is now" age, "!");
 
-//let people = 10;
-//addition people = people +1;
-//subtraction people = people -1;
-//multiplication people = people * 2
-//divisionpeople = people /2
-//let extrapeople = people % 1;
-//shorter way of writing above line people += 1 
-//multiplication *=
-//division /=
-//addition +=
-//subtraction -=
+let x;
+let y;
+let z;
 
+x = Number("3.14");
+y = String(3.14);
+z = Boolean("test");
 
-//console.log(extrapeople)
-/*
-  operator precedence
-  1. parenthesis ()
-  2. exponents
-  3. multiplication & division
-  4. addition & subtraction
-*/
-//let result = 1 + 2 * 3 + 4
-//console.log(result)
-
-
-
-//user input
-
-//ez way(window prompt)
-
-//let username = window.prompt("Enter your username")
-
-
-
-//hard way (html elements)
-
-
-//submit functionality
-
-
-let username;
-
-document.getElementById("Button").onclick = function(){
-    
-    username = document.getElementById("Text").value;
-    console.log(username);
-    document.getElementById("Label").innerHTML = "Hello " + username;
-}
-
-console.log(username)
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
