14eaf3e6d517173545682554ef5172a782214bb6
Cow Lols <121976328+CowLols@users.noreply.github.com>
2023-02-05 13:34:22 -0500

    arithmetic expression

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ a
 a
 a
 */
+
+
 //let count = 64; //number
 //let name = "Box"; //string
 //let heavy = true; //booleans
@@ -25,3 +27,29 @@ a
 //document.getElementById("p1".innerhtml = "item:" + name);
 //document.getElementById("p2".innerhtml = "count: " + count);
 //document.getElementById("p3".innerhtml = "heavy?" +  heavy);
+
+//arithmetic expression
+
+//let people = 10;
+//addition people = people +1;
+//subtraction people = people -1;
+//multiplication people = people * 2
+//divisionpeople = people /2
+//let extrapeople = people % 1;
+//shorter way of writing above line people += 1 
+//multiplication *=
+//division /=
+//addition +=
+//subtraction -=
+
+
+//console.log(extrapeople)
+/*
+  operator precedence
+  1. parenthesis ()
+  2. exponents
+  3. multiplication & division
+  4. addition & subtraction
+*/
+//let result = 1 + 2 * 3 + 4
+//console.log(result)
