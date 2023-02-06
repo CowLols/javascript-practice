ce6855e7eb9c8bf49af27c2b531f1b0f5d7b9174
Cow Lols <121976328+CowLols@users.noreply.github.com>
2023-02-05 19:43:17 -0500

    button counter (fixed)

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ let count = 0
 
 document.getElementById("decreasebtn").onclick = function(){
     count-=1
+}
     document.getElementById("countlabel").innerHTML;
-
-}
-
-document.getElementById("resetbtn").onclick = function(){
-    count=0
-    document.getElementById("countlabel").innerHTML
- 
-}
-
-document.getElementById("increasebtn").onclick = function(){
-    count+=1
-    document.getElementById("countlabel").innerHTML
- 
-}
+    
+    document.getElementById("resetbtn").onclick = function(){
+        count=0
+        document.getElementById("countlabel").innerHTML
+    
+    }
+    
+    document.getElementById("increasebtn").onclick = function(){
+        count+=1
+        document.getElementById("countlabel").innerHTML
+    }
