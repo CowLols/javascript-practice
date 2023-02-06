fa2cc936e9ef24d3f8e93b4768f10687bd36d132
Cow Lols <121976328+CowLols@users.noreply.github.com>
2023-02-05 20:05:51 -0500

    button counter (actually works this time)

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 
 
-let x;
-let y;
-let z;
+let count = 0;
 
-
-
-
-
-document.getElementById("rollbtn").onclick=function(){
-     x = Math.floor(Math.random() * 6) + 1;
-     y = Math.floor(Math.random() * 6) + 1;
-     z = Math.floor(Math.random() * 6 )+ 1;
-    
-    document.getElementById("xlabel").InnerHTML = x;
-    document.getElementById("ylabel").InnerHTML = y;
-    document.getElementById("zlabel").InnerHTML = z;
+document.getElementById("decreasebtn").onclick = function(){
+    count-=1;
+    document.getElementById("countlabel").innerHTML = count;
 }
+
+document.getElementById("resetbtn").onclick = function(){
+    count=0;
+    document.getElementById("countlabel").innerHTML = count;
+}
+
+document.getElementById("increasebtn").onclick = function(){
+    count+=1;
+    document.getElementById("countlabel").innerHTML = count;
+}
+
