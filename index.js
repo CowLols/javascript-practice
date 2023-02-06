7e07650ffbf0840ee1f442c5d4c9cfffb3ae75ea
Cow Lols <121976328+CowLols@users.noreply.github.com>
2023-02-05 20:50:34 -0500

    useful string methods

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
+// useful string methods
 
-let x;
-let y;
-let z;
+let username = "New User";
+let id = "923977532532804"
+//console.log(username.length); //character amount
+//console.log(firstletter = username.charAt(0));
+//username.indexOf("e"); //index of the first or last occurence of a character
+//console.log(username.lastIndexOf("e")); //last occurence of "e"
+//username = username.trim(); //trim a username
+//username = username.toUpperCase(); uppercase
+//username = username.toLowerCase(); lowercase
+id = id.replace ("8", "7"); //replaces 8 with 7.
 
-
-
-document.getElementById("rollbtn").onclick = function(){
-
-    x = Math.floor(Math.random() * 6) + 1;
-    y = Math.floor(Math.random() * 6) + 1;
-    z = Math.floor(Math.random() * 6) + 1;
-
-    document.getElementById("xlabel").innerHTML = x;
-    document.getElementById("ylabel").innerHTML = y;
-    document.getElementById("zlabel").innerHTML = z;
-}
+console.log(id);
