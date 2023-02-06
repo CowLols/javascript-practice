b4510644d115bb2c072feb1965013fc669fdd47a
Cow Lols <121976328+CowLols@users.noreply.github.com>
2023-02-06 15:22:56 -0500

    checked property (radio buttons)

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-//checked property 
-
-
 
 document.getElementById("button").onclick = function(){
-    if(document.getElementById("checkbox").checked == true){
-        console.log("Clicked!")
-        
 
-    }
-    else(console.log("Not clicked!"))
+  const checkbox = document.getElementById("checkbox")
+  const visa = document.getElementById("visa")
+  const mastercard = document.getElementById("mastercard")
+  const paypal = document.getElementById("paypal")
 
+  if(checkbox.checked){
+    console.log("clicked!")
+  }
+  else{
+    console.log("not clicked!")
+  }
 
+  if(visa.checked){
+    console.log("you are using visa.")
+  }
+  else if(mastercard.checked){
+    console.log("you are using mastercard.")
+  }
+  else if(paypal.checked){
+    console.log("you are usig paypal.")
+  }
+  else{
+    console.log("you haven't selected anything.")
+  }
 }
