9acff120d441b119a7ff618f3493f1bb8c487fd0
Cow Lols <121976328+CowLols@users.noreply.github.com>
2023-02-05 19:07:45 -0500

    hypotenuse triangle calculation

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-//math - :nerd:
+let a;
+let b;
+let c;
 
-let x = 1294.6;
-let y = 5;
-let z = 10;
-
-let maximum;
-let minimum;
-//x = Math.round(x);
-//x = Math.floor(x); round down
-//x = Math.ceil(x); round up
-//x = Math.pow(x); round to the nearest power
-//x = Math.sqrt(x); square root
-//x = Math.abs(x); absolute value
-//maximum = Math.max(x, y, z);
-//minimum = Math.min (x, y, z);
+a = window.prompt("Enter side A");
+a = Number(a);
 
 
+b = window.prompt("Enter side B");
+b = Number(b);
 
-console.log(x);
+c = Math.pow(a, 2) + Math.pow(b, 2);
+c = Math.sqrt(c);
+
+console.log("Side C:", c);
