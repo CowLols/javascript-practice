7c7c43befc3685af0673881d39fad5d2c8ae6331
Cow Lols <121976328+CowLols@users.noreply.github.com>
2023-02-06 14:28:38 -0500

    method chaining

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-//slice extracts a section of a string and returns it as
-//a new string without modifying the original string
+//method chaining - calling one method after another in one line of code.
+let username = "Arghh";
 
-let firstname = "frank";
-let lastname = "truckin";
-let fullname = "frank truckin";
-//firstname = fullname.slice(0, 5);
-//lastname = fullname.slice(6);
-console.log(firstname);
-console.log(lastname);
-firstname = fullname.slice(0,6)
-lastname = fullname.slice(fullname.indexOf(" ") + 1);
+//let letter = username.charAt(0);
+//letter = letter.toUpperCase ;
+
+
+
+
+let letter = username.charAt(0).toUpperCase().trim();
+
+
+console.log(letter);
