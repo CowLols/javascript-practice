6bcd71b39540b46fceda3d9b855f0ce02dfd5e3f
Cow Lols <121976328+CowLols@users.noreply.github.com>
2023-02-05 19:59:06 -0500

    random numbers

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-let count = 0
 
-document.getElementById("decreasebtn").onclick = function(){
-    count-=1
+
+let x;
+let y;
+let z;
+
+
+
+
+
+document.getElementById("rollbtn").onclick=function(){
+     x = Math.floor(Math.random() * 6) + 1;
+     y = Math.floor(Math.random() * 6) + 1;
+     z = Math.floor(Math.random() * 6 )+ 1;
+    
+    document.getElementById("xlabel").InnerHTML = x;
+    document.getElementById("ylabel").InnerHTML = y;
+    document.getElementById("zlabel").InnerHTML = z;
 }
-    document.getElementById("countlabel").innerHTML;
-    
-    document.getElementById("resetbtn").onclick = function(){
-        count=0
-        document.getElementById("countlabel").innerHTML
-    
-    }
-    
-    document.getElementById("increasebtn").onclick = function(){
-        count+=1
-        document.getElementById("countlabel").innerHTML
-    }
